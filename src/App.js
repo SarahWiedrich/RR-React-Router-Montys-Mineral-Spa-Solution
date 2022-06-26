3afc28782f28e570ae58f30423e40979226261ed
SarahWiedrich <sknapcik@gmail.com>
2022-06-26 10:41:38 -0500

    bootstrap styling added

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import './App.css';
 import Home from './components/Home'
 import About from './components/About'
 import Packages from './components/Packages'
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav'
 
 
 function App() {
@@ -17,20 +19,25 @@ function App() {
       <Router>
         <header>
           <h1 className="title">Welcome to Monty's Mineral SPA</h1>
-
-          <div className="navBar">
-            <ul>
-              <li>
-                <Link to="/">Home</Link>
-              </li>
-              <li>
-                <Link to="/about">About Us</Link>
-              </li>
-              <li>
-                <Link to="/packages">Our Packages</Link>
-              </li>
-            </ul>
-          </div>
+          <Container>
+            <Nav variant='tabs' fill>
+              <Nav.Item>
+                <Nav.Link>
+                  <Link to="/">Home</Link>
+                </Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link>
+                  <Link to="/about">About</Link>
+                </Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link>
+                  <Link to="/packages">Packages</Link>
+                </Nav.Link>
+              </Nav.Item>
+            </Nav>
+          </Container>
         </header>
 
         <div className="display">
--- src/components/Packages.js
+++ src/components/Packages.js
@@ -1,18 +1,34 @@
-
+import ListGroup from 'react-bootstrap/ListGroup'
+import Container from 'react-bootstrap/Container'
+import Card from 'react-bootstrap/Card'
 
 export default function Packages(props) {
-    const displayPackages = props.packages.map((eachPackage) => <li>{eachPackage}</li>)
+    const displayPackages = props.packages.map((eachPackage) => {
+        return <ListGroup.Item action variant="light">{eachPackage}</ListGroup.Item>
+    })
 
     return (
-        <div>
-            <div className="packages">
-                <div className="packagesHeader">
-                    <h1>Our Packages</h1>
-                </div>
-                <ul>
-                    {displayPackages}
-                </ul>
-            </div>
-        </div>
+       <Container>
+        <Card>
+            <Card.Body>
+                <Card.Title>Our Packages</Card.Title>
+                <Card.Text>Check out our packages!</Card.Text>
+            </Card.Body>
+        </Card>
+        <ListGroup>
+            {displayPackages}
+        </ListGroup>
+       </Container>
     )
 }
+
+{/* <div>
+<div className="packages">
+    <div className="packagesHeader">
+        <h1>Our Packages</h1>
+    </div>
+    <ul>
+        {displayPackages}
+    </ul>
+</div>
+</div> */}
